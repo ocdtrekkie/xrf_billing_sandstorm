62cf16f425001fd977a8ccfee4c7342a4b9b39c6
Jacob Weisz <inbox@jacobweisz.com>
2022-09-16 18:02:35 -0500

    Update scripts to Bullseye

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -22,7 +22,7 @@ HOME=/etc/mysql /usr/sbin/mysqld --initialize
 
 # Spawn mysqld, php
 HOME=/etc/mysql /usr/sbin/mysqld --skip-grant-tables &
-/usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf &
+/usr/sbin/php-fpm7.4 --nodaemonize --fpm-config /etc/php/7.4/fpm/php-fpm.conf &
 # Wait until mysql and php have bound their sockets, indicating readiness
 while [ ! -e /var/run/mysqld/mysqld.sock ] ; do
     echo "waiting for mysql to be available at /var/run/mysqld/mysqld.sock"
@@ -31,11 +31,12 @@ done
 if [ ! -e /var/.db-created ]; then
     mysql --user root -e 'CREATE DATABASE app'
     mysql --user root --database app < /opt/app/install_into_db.sql
+	mysql --user root --database app < /opt/app/install_into_db_billing.sql
     touch /var/.db-created
 fi
 
-while [ ! -e /var/run/php/php7.0-fpm.sock ] ; do
-    echo "waiting for php-fpm7.0 to be available at /var/run/php/php7.0-fpm.sock"
+while [ ! -e /var/run/php/php7.4-fpm.sock ] ; do
+    echo "waiting for php-fpm7.4 to be available at /var/run/php/php7.4-fpm.sock"
     sleep .2
 done
 
