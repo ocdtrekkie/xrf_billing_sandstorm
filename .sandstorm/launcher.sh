ad2bfeb4ae631633c2e8272261b77d9e8f5436c2
Jacob Weisz <inbox@jacobweisz.com>
2025-02-23 23:26:42 -0600

    Update launch script

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+
+wait_for() {
+    local service=$1
+    local file=$2
+    while [ ! -e "$file" ] ; do
+        echo "waiting for $service to be available at $file."
+        sleep .1
+    done
+}
+
 # Create a bunch of folders under the clean /var that php, nginx, and mysql expect to exist
 mkdir -p /var/lib/mysql
 mkdir -p /var/lib/mysql-files
@@ -16,18 +27,24 @@ rm -rf /var/tmp
 mkdir -p /var/tmp
 mkdir -p /var/run/mysqld
 
-# Ensure mysql tables created
-# HOME=/etc/mysql /usr/bin/mysql_install_db
-HOME=/etc/mysql /usr/sbin/mysqld --initialize
+# Rotate log files larger than 512K
+log_files="$(find /var/log -type f -name '*.log')"
+for f in $log_files; do
+    if [ $(du -b "$f" | awk '{print $1}') -ge $((512 * 1024)) ] ; then
+        mv $f $f.1
+    fi
+done
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+    # Ensure mysql tables created
+    HOME=/etc/mysql /usr/sbin/mysqld --initialize
+fi
 
 # Spawn mysqld, php
 HOME=/etc/mysql /usr/sbin/mysqld --skip-grant-tables &
 /usr/sbin/php-fpm7.4 --nodaemonize --fpm-config /etc/php/7.4/fpm/php-fpm.conf &
 # Wait until mysql and php have bound their sockets, indicating readiness
-while [ ! -e /var/run/mysqld/mysqld.sock ] ; do
-    echo "waiting for mysql to be available at /var/run/mysqld/mysqld.sock"
-    sleep .2
-done
+wait_for mysql /var/run/mysqld/mysqld.sock
 
 if [ ! -e /var/.db-2210.1 ]; then
 	if [ ! -e /var/.db-2209.1 ]; then
@@ -44,10 +61,7 @@ if [ ! -e /var/.db-2210.1 ]; then
 	touch /var/.db-2210.1
 fi
 
-while [ ! -e /var/run/php/php7.4-fpm.sock ] ; do
-    echo "waiting for php-fpm7.4 to be available at /var/run/php/php7.4-fpm.sock"
-    sleep .2
-done
+wait_for php-fpm7.4 /var/run/php/php7.4-fpm.sock
 
 # Start nginx.
 /usr/sbin/nginx -c /opt/app/.sandstorm/service-config/nginx.conf -g "daemon off;"
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.0.7
+
+- Improved input form validation.
+- Made more order details editable.
+
 # 0.0.6
 
 - Added inventory management panel.
